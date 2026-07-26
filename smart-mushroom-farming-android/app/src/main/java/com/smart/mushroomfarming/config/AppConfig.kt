eb60369b650111b86123890aa944ea21727a44b2
Riwitika <riwitikagupta@gmail.com>
2026-07-27 00:23:50 +0530

    Update BASE_URL in AppConfig.kt to production render URL

--- smart-mushroom-farming-android/app/src/main/java/com/smart/mushroomfarming/config/AppConfig.kt
+++ smart-mushroom-farming-android/app/src/main/java/com/smart/mushroomfarming/config/AppConfig.kt
@@ -1,5 +1,5 @@
 package com.smart.mushroomfarming.config
 
 object AppConfig {
-    const val BASE_URL = "http://192.168.0.73:8000/"
+    const val BASE_URL = "https://smart-mushroom-farming.onrender.com/"
 }
